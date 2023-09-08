b05d15f5c042a7ab1f4204b7c8e5554c83e147c7
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-09-08 11:33:59 -0600

    Maxwell Christian

---
+++ src/lo_decision_statements/DivisibleBy5.java
@@ -0,0 +1,20 @@
+package lo_decision_statements;
+
+// Write a program which takes a number from the user and checks whether the number is
+// divisible by 5 or not
+
+public class DivisibleBy5 {
+
+    public static void main(String []args){
+
+        // declaration
+
+        // input
+
+        // processing
+
+        // output
+
+    }
+
+}
---
+++ src/lo_decision_statements/IsFactor.java
@@ -0,0 +1,22 @@
+package lo_decision_statements;
+
+// Write a program which takes input of TWO numbers from user and
+// checks if the second number is a factor of first number or not
+
+// Hint: a number is a factor if the number is divisible by that factor
+
+public class IsFactor {
+
+    public static void main(String []args){
+
+        // declaration
+
+        // input
+
+        // processing
+
+        // output
+
+    }
+
+}
