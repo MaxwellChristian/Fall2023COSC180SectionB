6af99a4e84cab00d9530e04dd58c778b72bb4e02
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-09-08 10:50:22 -0600

    Maxwell Christian

---
+++ src/lo_decision_statements/PositiveNegativeCheck.java
@@ -0,0 +1,34 @@
+package lo_decision_statements;
+
+/*
+Write a program which takes input of a number from the user and
+displays whether the number is positive or negative
+ */
+
+import java.util.Scanner;
+
+public class PositiveNegativeCheck {
+
+    public static void main(String []args) {
+
+        // declaration section
+        double number;
+
+        // input section
+        Scanner inputLine = new Scanner(System.in);
+
+        // prompt the user for input of a number
+        System.out.print("Enter any number: ");
+        number = inputLine.nextDouble();
+
+        // check and display whether the number is positive or negative
+        if (number > 0) {
+            System.out.println(number + " : positive");
+        }
+        else {
+            System.out.println(number + " : negative");
+        }
+
+    }
+
+}
