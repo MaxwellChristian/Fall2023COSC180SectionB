8813220ab08945f0a23bf5a014a6f7456883c420
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-09-22 12:29:28 -0600

    Maxwell Christian

---
+++ src/lo_arrays/CombineArrays.java
@@ -0,0 +1,20 @@
+package lo_arrays;
+
+/*
+* Write a program which takes input of 2 numbers from user.
+* Both the numbers must  be different.
+* Create two different arrays as per the two input numbers.
+* Then the program generates random numbers and stores them
+* in the two arrays.
+* The program should then create a 3rd array and
+* store the values from array1 followed by array2 in array3
+*
+* Ex:
+* num1 : 5
+* num2 : 7
+* arr1 : 10, 78, 32, 45, 11
+* arr2 : 100, 78, 75, 11, 11, 78, 75
+* arr3 : 10, 78, 32, 45, 11, 100, 78, 75, 11, 11, 78, 75
+* */
+public class CombineArrays {
+}
---
+++ src/lo_arrays/SearchFromArray.java
@@ -0,0 +1,19 @@
+package lo_arrays;
+
+/*
+* Write a program which generates 100 random numbers
+* int the range of 200 and 700.
+* It then takes an input of a number from the user.
+* Then it searches for the input number in the list of
+* generated random numbers and
+* if the number is present, returns the position of the input number,
+* if not present, returns a message "Not found"
+* */
+
+public class SearchFromArray {
+
+    public static void main(String []args){
+
+    }
+
+}
