da4b6e6f92d7a682e5639d26c31dcb6ed7d231ef
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-09-29 11:31:34 -0600

    Maxwell Christian

--- src/lo_methods/SumOfDigits.java
+++ src/lo_methods/SumOfDigits.java
@@ -20,10 +20,47 @@ public class SumOfDigits {
         value = inputLine.nextInt();
 
         // logic to calculate the sum of all digits
-        int sumOfAllDigits = computeSumOfDigitis(value);
+        int sumOfAllDigits = computeSumOfDigits(value);
 
         System.out.println(value + " : sum of digits : " + sumOfAllDigits);
 
+        System.out.println(value + " : count of digits : " + countDigits(value)  );
+
+        System.out.println(value + " : reversed : " + reverse(value)  );
+
+    }
+
+    static int reverse(int value){
+        return 0;
+    }
+
+    private static int countDigits(int value) {
+        int count;
+
+//        for( count = 0 ; value >0 ; value/=10, count++ ){
+//
+//        }
+
+        for( count = 0 ; value > 0 ; value/=10, count++ );
+        {
+            // this will be not a part of loop, as
+            // the loop has semi-colon at the end
+            // the code in curly-braces will be executed once after
+            // the loop has completed
+        }
+        // observe the semi-colon at the end of the loop
+        // it represents that the loop is empty
+
+        return count;
+    }
+
+    private static int computeSumOfDigits(int value) {
+        int sum = 0;
+        while (value>0) {
+            sum += ( value%10 );
+            value /= 10;
+        }
+        return sum;
     }
 
 }
