1d33abfa591f83719cf33d68d75f7073f56eb73b
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-10-06 10:37:59 -0600

    Maxwell Christian

--- src/lo_methods/StringTokenizerDemo.java
+++ src/lo_methods/StringTokenizerDemo.java
@@ -7,7 +7,7 @@ public class StringTokenizerDemo {
     public static void main(String []args){
 
         String text = "Welcome to JAVA. JAVA    is easy if practiced.";
-        StringTokenizer tokenizer = new StringTokenizer(text);
+        StringTokenizer tokenizer = new StringTokenizer(text, " ");
 
         System.out.println("Total spaces : " + (tokenizer.countTokens()-1) );
 
@@ -17,8 +17,19 @@ public class StringTokenizerDemo {
 
             token = token.trim();
 
-            // System.out.println( token );
-            System.out.println( new StringBuilder(token).reverse() ); // displaying the reverse
+            System.out.println( token );
+            //System.out.println( new StringBuilder(token).reverse() ); // displaying the reverse
+
+        }
+
+        text = "1,     Maxwell,,,Christian    ,,CST";
+        tokenizer = new StringTokenizer(text, ",");
+        while (tokenizer.hasMoreTokens()){
+            String token = tokenizer.nextToken();
+
+            token = token.trim();
+
+            System.out.println( token );
 
         }
 
