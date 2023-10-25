ba75fd4ef7d6c6bfca35283450dde5bccc607751
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-10-25 11:53:19 -0600

    Maxwell Christian

--- src/lo_methods/ArraysAndMethods.java
+++ src/lo_methods/ArraysAndMethods.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 
 public class ArraysAndMethods {
 
-    public static void main(String []args) {
+    public static void main(String[] args) {
 
         //int []values = {1, 0, 1, 1, 1, 0, 1};
 
         // invoke/call a method which will return an array filled with random values
-        int []values = generateRandomArray(20, 2);
+        int[] values = generateRandomArray(20, 2);
 
         // print the generated array
         System.out.println(Arrays.toString(values));
@@ -18,34 +18,51 @@ public class ArraysAndMethods {
         values = generateRandomArray(20, maxValueRequired);
         System.out.println(Arrays.toString(values));
 
-        if( checkInArray(values, 12) ){
+        if (indexOf(values, 12) != -1) {
             System.out.println("Array contains 12");
-        }
-        else {
+        } else {
             System.out.println("Array does not contain 12");
         }
 
         // write a method which will return the position where a
         // value is found in the array
+        int foundAtPosition = indexOf(values, 12);
+        if (foundAtPosition != -1) {
+            System.out.println("12 exists at: " + foundAtPosition);
+        } else {
+            System.out.println("12 not found in the array");
+        }
+
+        int lastIndex = lastIndexOf(values, 12);
 
     }
 
-    private static boolean checkInArray(int[] values, int valueTocheck) {
-
-        for( int position = 0 ; position < values.length ; position++ ){
-            if( values[position] == valueTocheck ){
-                return true;
+    private static int lastIndexOf(int[] values, int valueToCheck) {
+        for (int position = values.length - 1; position >= 0; position--) {
+            if (values[position] == valueToCheck) {
+                return position;
             }
         }
 
-        return false;
+        return -1;
+    }
+
+    private static int indexOf(int[] values, int valueToCheck) {
+
+        for (int position = 0; position < values.length; position++) {
+            if (values[position] == valueToCheck) {
+                return position;
+            }
+        }
+
+        return -1;
     }
 
     private static int[] generateRandomArray(int totalValuesToGenerate, int maxValueRequired) {
 
-        int []values = new int[totalValuesToGenerate];
+        int[] values = new int[totalValuesToGenerate];
 
-        for( int position = 0 ; position < totalValuesToGenerate ; position++ ){
+        for (int position = 0; position < totalValuesToGenerate; position++) {
             values[position] = (int) (Math.random() * maxValueRequired);
         }
         return values;
