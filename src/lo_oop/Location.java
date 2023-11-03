8de7c4868f3d8aaae1232c1989355ab02d4649e1
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-11-03 11:53:06 -0600

    Maxwell Christian

--- src/lo_oop/Location.java
+++ src/lo_oop/Location.java
@@ -19,6 +19,15 @@ The return value is an instance of Location.
 Write a test program that prompts
 the user to enter a two-dimensional array and displays the location of the largest
 element in the array.
+
+Sampel run:
+Enter the number of rows and columns in the array: 3 4
+Enter the array:
+23.5 35 2 10
+4.5 3 45 3.5
+35 44 5.5 9.6
+The location of the largest element is 45 at (1, 2)
+
  */
 public class Location {
 }
