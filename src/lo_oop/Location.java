776b350b0c58182228339e03f6d3c422172c569c
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-11-03 11:51:51 -0600

    Maxwell Christian

---
+++ src/lo_oop/Location.java
@@ -0,0 +1,24 @@
+package lo_oop;
+
+
+/*
+(The Location class) Design a class named Location for locating a maximal
+value and its location in a two-dimensional array.
+
+The class contains public data
+fields row, column, and maxValue that store the maximal value and its indices in
+a two-dimensional array with row and column as int types and maxValue as a
+double type.
+
+Write the following method that returns the location of the largest element in a
+two-dimensional array:
+public static Location locateLargest(double[][] a)
+
+The return value is an instance of Location.
+
+Write a test program that prompts
+the user to enter a two-dimensional array and displays the location of the largest
+element in the array.
+ */
+public class Location {
+}
