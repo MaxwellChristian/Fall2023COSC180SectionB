5247169c3a5eb09bf65c3c614c4aabccf932c639
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-01-03 11:06:10 -0600

    Maxwell Christian

--- src/TestDivison.java
+++ src/TestDivison.java
@@ -3,6 +3,7 @@
 * Take two numbers from user and display the quotient of division operation
 * */
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TestDivison {
@@ -16,17 +17,19 @@ public class TestDivison {
 
         Scanner inputLine = new Scanner(System.in);
 
-        System.out.print("Enter num1 : ");
-        num1 = inputLine.nextInt();
-
-        System.out.print("Enter num2 : ");
-        num2 = inputLine.nextInt();
-
         try {
+            System.out.print("Enter num1 : ");
+            num1 = inputLine.nextInt();
+
+            System.out.print("Enter num2 : ");
+            num2 = inputLine.nextInt();
+
             ans = num1 / num2;
             System.out.printf("%d / %d : %d\n", num1, num2, ans);
         } catch ( ArithmeticException ex ) {
             System.out.println("Number 2 must be NON-ZERO");
+        } catch ( InputMismatchException ex ) {
+            System.out.println("Inputs must only be INT");
         }
 
     }
