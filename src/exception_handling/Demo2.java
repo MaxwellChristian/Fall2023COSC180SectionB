3692ee7dfdeef3a93fa005876e9bb4e018a3ccbb
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-01-04 09:59:59 -0600

    Maxwell Christian

--- src/exception_handling/Demo2.java
+++ src/exception_handling/Demo2.java
@@ -1,13 +1,13 @@
 package exception_handling;
 
 /*
-* Write a program to
-*   - take a number as input from user and use it as an array size
-*   - create an array of size specified as input from user
-*   - initialize the array with random int upto 5
-*   - display the quotient when dividing the first index with second
-*   - repeat the division for each possible index
-* */
+ * Write a program to
+ *   - take a number as input from user and use it as an array size
+ *   - create an array of size specified as input from user
+ *   - initialize the array with random int upto 5
+ *   - display the quotient when dividing the first index with second
+ *   - repeat the division for each possible index
+ * */
 
 import java.util.InputMismatchException;
 import java.util.Random;
@@ -15,45 +15,48 @@ import java.util.Scanner;
 
 public class Demo2 {
 
-    public static void main( String []args ) {
+    public static void main(String[] args) {
 
         int arrSize;
-        int []arr;
+        int[] arr;
 
         Scanner inputLine = new Scanner(System.in);
 
         try {
 
             // input the arr size
-            System.out.print("Enter arr size: ") ;
+            System.out.print("Enter arr size: ");
             arrSize = inputLine.nextInt();
 
             // create the array as per size
             arr = new int[arrSize];
 
             // initialize the arr [random values upto 5]
-            for (int i = 0 ; i < arr.length ; i++) {
+            for (int i = 0; i < arr.length; i++) {
                 arr[i] = new Random().nextInt(5);
             }
 
             // divide value at each index by the value at next index
-            for (int i = 0 ; i < arr.length - 2 ; i+=2) {
+            for (int i = 0; i < arr.length - 2; i += 2) {
 
                 try {
-                    int ans = arr[i] / arr[i+1];
-                    System.out.printf("%d / %d: %d\n", arr[i], arr[i+1], ans);
-                } catch ( ArithmeticException ex ) {
-                    System.out.printf("Cannot divide by zero: Skipped : %d / %d\n", arr[i], arr[i+1]);
+                    int ans = arr[i] / arr[i + 1];
+                    System.out.printf("%d / %d: %d\n", arr[i], arr[i + 1], ans);
+                } catch (ArithmeticException ex) {
+                    System.out.printf("Cannot divide by zero: Skipped : %d / %d\n", arr[i], arr[i + 1]);
                 }
 
             }
 
             System.out.println("The program execution completed");
 
-        } catch ( NegativeArraySizeException ex ) {
+        } catch (NegativeArraySizeException ex) {
             System.out.println("Array size must be POSITIVE value");
         } catch ( InputMismatchException ex ) {
             System.out.println( "Array size must be a POSITIVE INT value" );
+        } catch (Exception ex) {
+            // should be the last catch in the sequence of catch clauses
+            System.out.println("Exception generated: " + ex.getMessage());
         } finally {
             System.out.println("Bye");
         }
