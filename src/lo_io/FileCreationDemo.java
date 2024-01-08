d1c9471cdaffe0cd6f656401c9aac0b30efe95cb
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-01-08 14:45:54 -0600

    Maxwell Christian

--- src/lo_io/FileCreationDemo.java
+++ src/lo_io/FileCreationDemo.java
@@ -1,53 +1,75 @@
 package lo_io;
 
 /*
-* Input the name of the file from the user and
-* an int representing FILE or DIR and then
-* create the file/dir, if it does not exist
-* */
+ * Input the name of the file from the user and
+ * an int representing FILE or DIR and then
+ * create the file/dir, if it does not exist
+ * */
 
 import java.io.File;
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class FileCreationDemo {
 
-    public static void main(String []args) {
+    private static final int CREATE_FILE = 1;
+    private static final int CREATE_DIR = 2;
+
+    public static void main(String[] args) {
 
         String path = "data_files";
 
         Scanner inputLine = new Scanner(System.in);
 
-        System.out.println("Enter file name: ");
-        String fileName = inputLine.next();
+        try {
 
-        System.out.print("Create File[1] or Directory[2]: ");
-        int fileType = inputLine.nextInt();
+            System.out.println("Enter file name: ");
+            String fileName = inputLine.next();
 
-        switch (fileType){
-            case 1:
-                File fObj = new File(path + "\\" + fileName);
-                try {
-                    if(fObj.createNewFile()){
-                        System.out.println("File created");
+            System.out.print("Create File[1] or Directory[2]: ");
+            int fileType = inputLine.nextInt();
+
+            File fObj = new File(path + "/" + fileName);
+
+            switch (fileType) {
+                case CREATE_FILE:
+
+                    if( fObj.exists() ){
+                        System.out.println("File already exists");
                     }
                     else {
-                        System.out.println("File creation failed");
+                        try {
+                            if (fObj.createNewFile()) {
+                                System.out.println("File created");
+                            } else {
+                                System.out.println("File creation failed");
+                            }
+                        } catch (IOException e) {
+                            System.out.println("Oops. Something went wrong when creating the file");
+                        }
                     }
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                break;
-            case 2:
-                File fObj2 = new File(path + "\\" + fileName);
-                 if(fObj2.mkdir()){
-                     System.out.println("Directory created");
-                 }
-                 else {
-                     System.out.println("Directory creation failed");
-                 }
-                break;
+                    break;
+                case CREATE_DIR:
+
+                    if( fObj.exists() ){
+                        System.out.println("Directory already exists");
+                    }
+                    else {
+                        if (fObj.mkdir()) {
+                            System.out.println("Directory created");
+                        } else {
+                            System.out.println("Directory creation failed");
+                        }
+                    }
+
+                    break;
+            }
+
+        } catch (InputMismatchException immEx) {
+            System.out.println("Please provide the name as single word and then a valid int for the file type");
         }
+
 
     }
 
