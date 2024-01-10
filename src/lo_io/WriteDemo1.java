e449b2a77a447d8bd160bbe2daa897646a80b4d1
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-01-10 11:52:08 -0600

    Maxwell Christian

---
+++ src/lo_io/WriteDemo1.java
@@ -0,0 +1,36 @@
+package lo_io;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class WriteDemo1 {
+
+    public static void main(String []args){
+
+        String path = "data_files/test.txt";
+
+        // create an object of file
+        File fObj = new File(path);
+
+
+        try {
+            // use a writer/output stream to save data in the file
+            PrintWriter printWriter = new PrintWriter(fObj);
+
+            // save/write the data using the writer
+            printWriter.println("Welcome");
+            printWriter.println(57);
+            printWriter.println('A');
+            printWriter.println(89.21);
+
+            // close the file
+            printWriter.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println( fObj.getName() + ": No such path exists");
+        }
+
+    }
+
+}
