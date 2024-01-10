3fe310dd7c020a88d20b16f62edb52208db1fc95
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-01-10 11:04:39 -0600

    Maxwell Christian

---
+++ src/lo_io/PathStats.java
@@ -0,0 +1,15 @@
+package lo_io;
+
+/*
+* Write a program which take multiple paths as command line arguments and
+*   displays the size if the path refers to a file or
+*   displays the count of files and sub - dirs if the path refers to a dir
+* */
+
+public class PathStats {
+
+    public static void main(String []args) {
+
+    }
+
+}
