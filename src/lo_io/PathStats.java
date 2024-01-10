e92c6a462da7d9db08d8c0f1f9e3bbebaf0f0527
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-01-10 11:28:46 -0600

    Maxwell Christian

--- src/lo_io/PathStats.java
+++ src/lo_io/PathStats.java
@@ -6,9 +6,49 @@ package lo_io;
 *   displays the count of files and sub - dirs if the path refers to a dir
 * */
 
+import java.io.File;
+
 public class PathStats {
 
     public static void main(String []args) {
+
+        // iterate over each command line argument
+        for (String curArg: args) {
+//            System.out.println(curArg);
+
+            // create a file object referring to the path as per current parameter
+            File fObj = new File(curArg);
+
+            if( fObj.exists() ){
+                if( fObj.isFile() ){
+                    System.out.println("F: " + fObj.getName() + " : " + fObj.length());
+                } else {
+                    if( fObj.isDirectory() ){
+                        // System.out.println("D: " + fObj.getName() + " : " + fObj.length());
+
+                        // display count of total files and total sub-dirs
+                        int totalFiles = 0;
+                        int totalDirs = 0;
+                        for( File f: fObj.listFiles()){
+                            if( f.isFile() ){
+                                totalFiles++;
+                            } else {
+                                if(f.isDirectory()){
+                                    totalDirs++;
+                                }
+                            }
+                        }
+
+                        System.out.print(fObj.getName() + " -> ");
+                        System.out.println("Total files: " + totalFiles + ", Total dirs: " + totalDirs);
+                    }
+                }
+            } else {
+                System.out.println(curArg + " : No such file exists");
+            }
+
+
+        }
 
     }
 
