0a43760ea3325073e431f4add182174dd03e8e41
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-01-17 11:15:31 -0600

    Maxwell Christian

---
+++ src/lo_recursion/Sum1ToN.java
@@ -0,0 +1,14 @@
+package lo_recursion;
+
+/*
+* Write a recursive method which can generate
+* summation of 1 to N. N passed as parameter to the method.
+* */
+
+public class Sum1ToN {
+
+    public static void main(String []args) {
+
+    }
+
+}
