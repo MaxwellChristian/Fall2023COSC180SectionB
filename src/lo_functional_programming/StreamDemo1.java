94275eb9bc45aba2a57875ecf3a63cde03b2f7fd
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-01-29 15:21:50 -0600

    Maxwell Christian

--- src/lo_functional_programming/StreamDemo1.java
+++ src/lo_functional_programming/StreamDemo1.java
@@ -1,5 +1,7 @@
 package lo_functional_programming;
 
+import lo_inheritance.multi_level.A;
+
 import java.util.Arrays;
 
 public class StreamDemo1 {
@@ -19,7 +21,7 @@ public class StreamDemo1 {
         int[] alValuesL50 = Arrays.stream(values).filter(value -> value > 50).toArray();
         System.out.println("Values > 50: " + Arrays.toString(alValuesL50));
 
-        String[] words = "welcome to the test of JAVA as JAVA is always easy to test".split(" ");
+        String[] words = "welcome to the test of JAVA As JAVA is always easy to test".split(" ");
         System.out.println("Words: " + Arrays.toString(words));
 
         // store first four words
@@ -58,20 +60,71 @@ public class StreamDemo1 {
 
         // display all words sorted [ignoring the case and not using the first 4 words]
 
+        // lambda : (o1, o2) -> o1.compareToIgnoreCase(o2)
+        System.out.println("Skipping first 4 words [sorted words ignoring case]: " +
+                Arrays.stream(words)
+                        .skip(4)
+                        .sorted(String::compareToIgnoreCase)
+                        .toList()
+        );
+
         // get the largest word with length >= 3
+        System.out.println("Largest word [with length >= 3]: " +
+                Arrays.stream(words)
+                        .filter(s -> s.length() >= 3)
+                        .max(String::compareTo)
+                        .get()
+                );
 
         // get the smallest word [alphabetically smallest]
+        System.out.println("Smallest word [alphabetically]: " +
+                Arrays.stream(words)
+                        .sorted()
+                        .min(String::compareToIgnoreCase)
+                        .get()
+                );
 
         // check and return the status of whether all names start with a capital alphabet
+        System.out.println("Are all words starting with capital: " +
+                        Arrays.stream(words)
+                                .allMatch(s -> Character.isUpperCase(s.charAt(0)))
+                );
+        // display all words not starting with capital
+        System.out.println("All words starting with capital: " +
+                Arrays.stream(words)
+                        .filter(s -> Character.isUpperCase(s.charAt(0)))
+                        .toList()
+        );
 
         // check and return the status where no word begins with JA
+        System.out.println("No words starting with 'JA': " +
+                Arrays.stream(words)
+                        .noneMatch(s -> s.startsWith("JA"))
+        );
 
         // get the first element in the stream
+        System.out.println("First element: " + Arrays.stream(words).findFirst().get());
 
         // display the first element of the stream in lowercase
+        // System.out.println("First element: " + Arrays.stream(words).findFirst().get().toLowerCase());
+        System.out.println("First element: " + Arrays.stream(words).map(String::toLowerCase).findFirst().get());
+
+        // display all words in lower case
+        System.out.println("All [lower case]: " + Arrays.stream(words).map(String::toLowerCase).toList());
 
         // write a method which takes words as parameters and returns an array consisting of all words in lowercase
+        String []lowerCaseWords = toLowerCase(words);
 
+        // display all words with length >= 3 in the format word:word_length
+        Arrays.stream(words)
+                .filter(s -> s.length() >= 3)
+                .forEach( s -> System.out.printf("%s: %d\n", s, s.length()) );
+
+    }
+
+    private static String[] toLowerCase(String[] words) {
+        // return (String[]) Arrays.stream(words).map(String::toLowerCase).toArray();
+        return Arrays.stream(words).map(String::toLowerCase).toArray(String[]::new);
     }
 
 }
