a47e18b6144c866bd3c8dadd1dce92fb8a54210c
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-01-31 12:02:13 -0600

    Maxwell Christian

--- src/lo_functional_programming/StreamDemo2.java
+++ src/lo_functional_programming/StreamDemo2.java
@@ -1,7 +1,9 @@
 package lo_functional_programming;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamDemo2 {
@@ -14,29 +16,51 @@ public class StreamDemo2 {
         System.out.println("Values: " + Arrays.toString(values));
 
         // adding each value i.e. ans = v1 + v2 ... + vN
-        System.out.println("Values [sum]: ");
+        System.out.println("Values [sum]: " + Arrays.stream(values).sum());
 
         // multiplying each value i.e. ans = v1 * v2 ... * vN
-        System.out.println("Values [mul]: ");
+        System.out.println("Values [mul]: " + Arrays.stream(values)
+                .reduce(1, (left, right) -> left * right));
 
         // display as CSV
-        System.out.println("Values [CSV]: ");
+        System.out.println("Values [CSV]: " +
+                Arrays.stream(values)
+                        .mapToObj(String::valueOf)
+                        .reduce((s1, s2) -> s1 + ", " + s2)
+                        .get()
+        );
 
 
         String[] names = {"John", "Peter", "Susan", "Kim", "Jen",
                 "George", "Alan", "Stacy", "Michelle", "john"};
 
         // count number of characters of all names
-        System.out.println("Number of characters in all names: ");
+        System.out.println("Number of characters in all names: " +
+                Arrays.stream(names)
+                        .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                        .length()
+        );
 
         // all names [into ArrayList]
-        System.out.println("In ArrayList [all names]: ");
+//        ArrayList<String> al = (ArrayList<String>) Arrays.stream(names)
+//                .toList();
+
+        System.out.println("In ArrayList [all names]: " +
+                Arrays.stream(names)
+                        .collect(ArrayList::new, ArrayList::add, ArrayList::addAll)
+        );
 
         // fetch a set of all ames [all names should be in upper case]
-        System.out.println("In Set [all names]: " );
+        System.out.println("In Set [all names]: " +
+                Arrays.stream(names)
+                        .map(String::toUpperCase)
+                        .collect(Collectors.toSet())
+        );
 
         // all names [into Map]
         System.out.println("In Map [all names]: "
+                + Arrays.stream(names)
+                .collect(Collectors.toMap(s -> s, String::length))
         );
 
         // count of all characters in all names
