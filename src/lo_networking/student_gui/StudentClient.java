9b64e18cdbc36bbb194f63c7954d302bdec325b7
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-03-20 10:58:08 -0600

    Maxwell Christian

---
+++ src/lo_networking/student_gui/StudentClient.java
@@ -0,0 +1,110 @@
+package lo_networking.student_gui;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class StudentClient extends Application {
+
+    TextField tfLastName;
+    TextField tfFirstName;
+    TextField tfCity;
+    TextField tfProvince;
+    TextField tfPostalCode;
+
+    Button btnSendToServer = new Button("Send");
+
+    static String serverAddress;
+    static int serverPort;
+
+    public static void main(String ... args) {
+
+        serverAddress = args[0];
+        serverPort = Integer.parseInt(args[1]);
+
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        GridPane gridPane = new GridPane(20, 20);
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+
+        // set up the controls
+        tfLastName = new TextField();
+        tfFirstName = new TextField();
+        tfCity = new TextField();
+        tfProvince = new TextField();
+        tfPostalCode = new TextField();
+
+        // add to the pane
+        gridPane.add(new Label("Enter last name: "), 0, 0);
+        gridPane.add(tfLastName, 1, 0);
+
+        gridPane.add(new Label("Enter first name: "), 0, 1);
+        gridPane.add(tfFirstName, 1, 1);
+
+        gridPane.add(new Label("Enter city: "), 0, 2);
+        gridPane.add(tfCity, 1, 2);
+
+        gridPane.add(new Label("Enter province: "), 0, 3);
+        gridPane.add(tfProvince, 1, 3);
+
+        gridPane.add(new Label("Enter postal code: "), 0, 4);
+        gridPane.add(tfPostalCode, 1, 4);
+
+        gridPane.add(btnSendToServer, 1, 5);
+        btnSendToServer.setOnAction(actionEvent -> sendToServer(serverAddress, serverPort));
+
+        // create a scene with the pane
+        Scene scene = new Scene(gridPane, 400, 500);
+
+        // add to stage
+        primaryStage.setScene(scene);
+
+        // show the stage
+        primaryStage.show();
+
+    }
+
+    private void sendToServer(String serverAddress, int serverPort) {
+
+        try {
+            Student studentToSend = getStudent();
+
+            Socket connectedServer = new Socket(serverAddress, serverPort);
+
+            ObjectOutputStream outputStreamToServer =
+                    new ObjectOutputStream(connectedServer.getOutputStream());
+            outputStreamToServer.writeObject(studentToSend);
+
+            connectedServer.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Student getStudent() {
+
+        Student student = new Student();
+
+        student.setLastName(tfLastName.getText());
+        student.setFirstName(tfFirstName.getText());
+        student.setCity(tfCity.getText());
+        student.setProvince(tfProvince.getText());
+        student.setPostalCode(tfPostalCode.getText());
+
+        return student;
+    }
+}
---
+++ src/lo_networking/student_gui/StudentServer.java
@@ -0,0 +1,90 @@
+package lo_networking.student_gui;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class StudentServer extends Application {
+
+    static int serverPort;
+
+    TextArea textArea;
+
+    public static void main(String ... args) {
+
+        serverPort = Integer.parseInt(args[0]);
+
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(textArea = new TextArea());
+
+        Scene scene = new Scene(borderPane, 450, 200);
+
+        primaryStage.setTitle("Area Server");
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+
+        Thread serverThread = new Thread(() -> startServer(serverPort));
+        serverThread.setDaemon(true);
+        serverThread.start();
+
+    }
+
+    private void startServer(int serverPort) {
+        try (ServerSocket serverSocket = new ServerSocket(serverPort)) {
+            Platform.runLater(() -> showMessage("Server started. Waiting for connections on port " + serverPort + "\n"));
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                Platform.runLater(() -> showMessage("Client connected: " + clientSocket.getInetAddress() + "\n"));
+
+                // Handle client connection in a separate thread
+                Thread connectedClientThread = new Thread(() -> handleClientConnection(clientSocket));
+                connectedClientThread.setDaemon(true);
+                connectedClientThread.start();
+            }
+        } catch (IOException e) {
+            Platform.runLater(() -> showMessage(String.valueOf(e)));
+        }
+    }
+
+    private void handleClientConnection(Socket clientSocket) {
+        try (ObjectInputStream inputStream = new ObjectInputStream(clientSocket.getInputStream())) {
+            while (true) {
+
+                Student receivedStudent = (Student) inputStream.readObject();
+
+                Platform.runLater(() -> {
+                    showMessage("Received student: " + receivedStudent + "\n");
+                });
+
+            }
+        } catch (IOException e) {
+            Platform.runLater(() -> showMessage("Client disconnected: "
+                    + clientSocket.getInetAddress()
+                    + ", Port" + clientSocket.getPort()
+                    + "\n"));
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void showMessage(String message) {
+        textArea.appendText(message);
+    }
+
+}
