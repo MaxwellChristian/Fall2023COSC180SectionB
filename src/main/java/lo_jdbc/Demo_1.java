4d2713aadf88673b37e21ed491ac5f88b992c453
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-03-22 12:17:38 -0600

    Maxwell Christian

--- src/main/java/lo_jdbc/Demo_1.java
+++ src/main/java/lo_jdbc/Demo_1.java
@@ -14,17 +14,21 @@ public class Demo_1 {
 
         try {
             // load the appropriate driver for the database used
-            // i.e. load the class which represents the driver
+            // i.e., load the class which represents the driver
             Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
             System.out.println("Driver loaded");
 
             // connect to database
-            Connection connection = DriverManager.getConnection("jdbc:ucanaccess://Students.accdb", "scott", "tiger");
+
+            // Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/javabook", "db-user-name", "db-password");
+
+            // Connection connection = DriverManager.getConnection("jdbc:ucanaccess://Students.accdb", "scott", "tiger");
+            Connection connection = DriverManager.getConnection("jdbc:ucanaccess://db_files/Students.accdb", "scott", "tiger");
             System.out.println("Database connected");
 
             // create the operation that you need to perform with the database
             // here we will fetch some records from the table "students"
-            String query = "SELECT room, level FROM students";
+            String query = "SELECT level, room FROM students";
 
             // to work with database commands, we need to create a 'Statement'
             Statement statement = connection.createStatement();
@@ -34,12 +38,17 @@ public class Demo_1 {
 
             // iterate over the received results
             while (resultSet.next()) {
-                System.out.println("Record traversed");
+                // System.out.println("Record traversed");
+
+                // display the columns from the fetched records
+                String fetchedColum1 = resultSet.getString(1);
+                int fetchedColum2 = resultSet.getInt(2);
+
+                System.out.println(fetchedColum1 + ", " + fetchedColum2);
             }
 
             // close the connection
             connection.close();
-
 
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
--- src/module-info.java
+++
@@ -1,7 +0,0 @@
-module Fall2023COSC180SectionB {
-    requires javafx.graphics;
-    requires javafx.controls;
-    requires java.sql;
-
-    exports gui;
-}
