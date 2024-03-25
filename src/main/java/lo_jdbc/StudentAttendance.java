e3779f71e2a7aaa9ceb0f223c955b85e37a4ba08
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-03-25 15:00:18 -0600

    Maxwell Christian

--- src/main/java/lo_jdbc/StudentAttendance.java
+++ src/main/java/lo_jdbc/StudentAttendance.java
@@ -1,20 +1,24 @@
 package lo_jdbc;
 
 /*
-* Display the attendance for each student.
-* The details must have the student ID, student name,
-* date of attendance and status of attendance
-*
-* */
+ * Display the attendance for each student.
+ * The details must have the student ID, student name,
+ * date of attendance and status of attendance.
+ *
+ * The attendance is to be fetched for the student whose ID is provided
+ * by the user when the program is executed.
+ *
+ * */
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Arrays;
 
 public class StudentAttendance {
 
-    public static void main(String []args) {
+    public static void main(String[] args) {
 
         try {
 
@@ -31,9 +35,39 @@ public class StudentAttendance {
             System.out.println("Statement created");
 
             // execute the database commands using statement
-            String sqlQuery = "SELECT level FROM Students";
+
+            String []columnsToFetch = { "LastName", "FirstName", "Student", "AttendanceDate", "Status" };
+            String []tablesToUse = {"Students", "StudentAttendance"};
+
+            String fieldList = Arrays.toString(columnsToFetch);
+            String tableList = Arrays.toString(tablesToUse);
+
+            System.out.println(fieldList.substring(1, fieldList.length()-1));
+            System.out.println(tableList.substring(1, tableList.length()-1));
+
+//            String sqlQuery =
+//                    "SELECT " +
+//                            "LastName, " +
+//                            "FirstName, " +
+//                            "Student, " +
+//                            "AttendanceDate, " +
+//                            "Status " +
+//                        "FROM " +
+//                            "Students, " +
+//                            "StudentAttendance " +
+//                        "WHERE " +
+//                            "Students.StudentID = StudentAttendance.Student";
+
+            String sqlQuery =
+                    "SELECT " +
+                            fieldList.substring(1, fieldList.length()-1) +
+                        " FROM " +
+                            tableList.substring(1, tableList.length()-1) +
+                        " WHERE " +
+                            "Students.StudentID = StudentAttendance.Student";
 
             ResultSet resultSet;
+            System.out.println("SQL to execute: " + sqlQuery);
             resultSet = statement.executeQuery(sqlQuery);
             System.out.println("SQL executed: " + sqlQuery);
 
@@ -42,8 +76,21 @@ public class StudentAttendance {
             while (resultSet.next()) {
 
                 // process the required columns
-                String fetchedColumn1 = resultSet.getString(1);
-                System.out.println("Fetched column's value: " + fetchedColumn1);
+                String studentID = resultSet.getString(3);
+                String attendingDate = resultSet.getString(4);
+                String attendanceStatus = resultSet.getString(5);
+
+                String studentName =
+                        resultSet.getString(1) +
+                                " " +
+                                resultSet.getString(2);
+
+                System.out.println(studentID
+                        + "(" + studentName + ")"
+                        + ": "
+                        + attendingDate
+                        + " -> "
+                        + attendanceStatus);
 
             }
             System.out.println("Display completed");
