0d11f53d69c8db468b8d4b18157b1545a3e02170
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-04-05 11:31:01 -0600

    Maxwell Christian

--- src/main/java/lo_jdbc/DBHelper.java
+++ src/main/java/lo_jdbc/DBHelper.java
@@ -6,22 +6,27 @@ import java.sql.SQLException;
 
 public class DBHelper {
 
-    static final public String DRIVER_SQLITE = "org.sqlite.JDBC";
-    static final public String DRIVER_ACCESS = "net.ucanaccess.jdbc.UcanaccessDriver";
+    static final private String DRIVER_SQLITE = "org.sqlite.JDBC";
+    static final private String DRIVER_ACCESS = "net.ucanaccess.jdbc.UcanaccessDriver";
 
     static final private String connectionSQLITE = "jdbc:sqlite:";
     static final private String connectionACCESS = "jdbc:ucanaccess://";
 
-    public static Connection connect(int databaseType, String databaseName) throws ClassNotFoundException, SQLException {
+    public enum DATABASE_TYPE {
+        SQLITE,
+        ACCESS
+    }
+
+    public static Connection connect(DATABASE_TYPE databaseType, String databaseName) throws ClassNotFoundException, SQLException {
 
         Connection connection = null;
 
         switch (databaseType) {
-            case 1:
+            case SQLITE:
                 Class.forName(DRIVER_SQLITE);
                 connection = DriverManager.getConnection(connectionSQLITE + databaseName);
                 break;
-            case 2:
+            case ACCESS:
                 Class.forName(DRIVER_ACCESS);
                 connection = DriverManager.getConnection(connectionACCESS + databaseName);
                 break;
